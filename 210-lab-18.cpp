62f96c89c82f5907de3761dfb4072c01c66e93c7
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-03 20:05:28 -0700

    10-minute update

--- 210-lab-18.cpp
+++ 210-lab-18.cpp
@@ -9,3 +9,33 @@
 // at program startup ask which mode to use (head or tail)
 // prompt user to enter data and store in linked list
 // after inputting all reviews, traverse linked list to output the data and calculate/output average review
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int listMethod;
+	double tempRating;
+	string tempComment;
+	char choice;
+
+	cout << "Which linked list method should we use?" << endl;
+	cout << "\t [1] New nodes are added to the head of the linked list" << endl;
+	cout << "\t [2] New nodes are added to the tail of the linked list" << endl;
+	cout << "\tChoice: ";
+	cin >> listMethod;
+
+	cout << "Enter review rating 0-5: ";
+	cin >> tempRating;
+	cout << "Enter review comments: ";
+	getline(cin, tempComment);
+	cin.ignore();
+	cout << "Enter another review? Y/N: ";
+	cin >> choice;
+
+	cout << "Outputting all reviews:" << endl;
+	cout << "\t> Review #" << ":" << endl;
+	cout << "\t> Average: ";
+
+	return 0;
+}
