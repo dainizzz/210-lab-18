3ff8d9221759a886381319d9d11386a0aec13df2
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-03 19:54:42 -0700

    Initial setup

---
+++ 210-lab-18.cpp
@@ -0,0 +1,11 @@
+// COMSC-210 | Lab 18 | Dainiz Almazan
+// IDE used: CLion
+
+// movie reviewer: watches a film, rates it, provides comments
+// write a program to store the reviews by reviewers
+// this will be a linked list w/ nodes having a rating and a comment
+// write code to add nodes to tail of linked list
+// also add code for adding nodes to head of linked list
+// at program startup ask which mode to use (head or tail)
+// prompt user to enter data and store in linked list
+// after inputting all reviews, traverse linked list to output the data and calculate/output average review
